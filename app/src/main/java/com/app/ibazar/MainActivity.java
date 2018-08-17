ccb36a19d3bfbb9abe29a2e089cf7660ca62ff04
Safnaj <37530024+Safnaj@users.noreply.github.com>
2018-08-18 00:11:23 +0530

    Uploaded

---
+++ app/src/main/java/com/app/ibazar/MainActivity.java
@@ -0,0 +1,21 @@
+package com.app.ibazar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void signup(View view){
+        Intent intent = new Intent(this,Registration.class);
+        startActivity(intent);
+    }
+
+}
